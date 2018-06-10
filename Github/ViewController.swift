30b1712d408682cf1e12d3cd43d7000e034c705e
Kevin He <3057469318@qq.com>
2018-06-10 10:49:07 +0800

    Initial Commit

---
+++ Github/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by kevinhe on 2018/6/9.
+//  Copyright © 2018年 kevinhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        print("world")
+        print("1")
+        print("2")
+        print("3")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
